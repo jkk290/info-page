9430642f094d05d8656389f92e5e627f08613dd1
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-22 10:30:58 -1000

    Refactor to implement express

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
-import http from 'node:http';
-import fs from 'node:fs/promises';
+const fs = require('node:fs');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const requestListener = async function (req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    switch (req.url) {
-        case '/':
-            try {
-                const aboutFile = await fs.readFile('./index.html');
-                res.writeHead(200);
-                res.end(aboutFile);
-                break
-            } catch (err) {
-                console.error(err);
-            }
-        case '/about':
-            try {
-                const aboutFile = await fs.readFile('./about.html');
-                res.writeHead(200);
-                res.end(aboutFile);
-                break
-            } catch (err) {
-                console.error(err);
-            }
-        case '/contact-me':
-            try {
-                const aboutFile = await fs.readFile('./contact-me.html');
-                res.writeHead(200);
-                res.end(aboutFile);
-                break
-            } catch (err) {
-                console.error(err);
-            }
-        default:
-            try {
-                const aboutFile = await fs.readFile('./404.html');
-                res.writeHead(200);
-                res.end(aboutFile);
-                break
-            } catch (err) {
-                console.error(err);
-            }
-    }
-}
+const app = express();
+const PORT = 3000;
 
-const server = http.createServer(requestListener);
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/about', (req, res) => res.sendFile(__dirname + '/about.html'));
+app.get('/contact-me', (req, res) => res.sendFile(__dirname + '/contact-me.html'));
+app.get('/*splat', (req, res) => res.sendFile(__dirname + '/404.html'));
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(PORT, () => {
+    console.log(`My Info app - listening on port ${PORT}!`);
 });
